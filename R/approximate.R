a9065b1d1abc0ad03dd9a5178d461acd4a30c9dd
Daniel Antal <antaldaniel@users.noreply.github.com>
2021-05-11 17:11:34 +0200

    approximation + unit tests

--- R/approximate.R
+++ R/approximate.R
@@ -2,8 +2,8 @@
 #'
 #' Approximation and other filling techniques require unique observations
 #' @param indicator An indicator table to test.
-#' @importFrom dplyr select group_by add_count
-#' @return \code{NULL} if the test is met, otherwise and error message.
+#' @importFrom dplyr select group_by add_count filter
+#' @return \code{TRUE} if the test is met, otherwise and error message.
 #' @export
 
 test_unique_observations <- function( indicator ) {
@@ -14,20 +14,42 @@ test_unique_observations <- function( indicator ) {
     add_count() %>%
     filter ( n != 1 )
 
-  if ( nrow(uniqueness)==0 ) return(NULL) else {
-    stop ( uniqueness ,  "\n is not unique. This is an error." )
+  if (nrow(uniqueness)==0) return(TRUE) else {
+    stop ( uniqueness,  "\n is not unique. This is an error." )
   }
+}
+
+#' Create Time Series Object
+#'
+#' Create a time series object from tmp in approximation functions.
+#' @param tmp A temporary indicator table created by an approximation function.
+#' @importFrom timetk tk_ts
+#' @importFrom dplyr case_when
+#' @importFrom lubridate ymd
+#' @return \code{TRUE} if the test is met, otherwise and error message.
+#' @keywords internal
+
+create_time_series <- function( tmp ) {
+
+  start_value <- lubridate::ymd(min(tmp$time))
+
+  timetk::tk_ts(tmp ,
+                start = start_value,
+                frequency =   case_when ( freq == "A" ~ 1,
+                                          freq == "Q" ~ 4,
+                                          freq == "M" ~ 12),
+                silent = TRUE)
 }
 
 
 #' Linear approximation of missing values
 #'
-#' @param indicator a character string with the indicator name
 #'
-#' @importFrom timetk tk_ts
-#' @importFrom lubridate year
-#' @importFrom tidyr pivot_wider
-#' @importFrom dplyr select filter left_join mutate case_when full_join anti_join
+#'
+#' @param indicator A tibble created by \code{\link{get_eurostat_indicator}}. with time, geo,
+#' value, frequency, estimate and method columns.
+#' @importFrom tidyr pivot_wider pivot_longer
+#' @importFrom dplyr select filter left_join mutate bind_cols
 #' @importFrom zoo na.approx
 #' @return A tibble updated with with approximated values.
 #' @export
@@ -39,102 +61,100 @@ na_approx <- function (indicator) {
   freq <- unique(indicator$frequency)
 
   tmp <- indicator %>%
-      select ( all_of(c("time", "geo", "value", "method"))) %>%
+      select ( all_of(c("time", "geo", "value", "frequency" ))) %>%
       pivot_wider( names_from = "geo",
                    values_from = "value")
 
-  start_value <- lubridate::year(min(tmp$time))
+  indicatot_ts <- create_time_series(tmp)
+  approximated <- zoo::na.approx(indicator_ts)
 
-  indy_ts <- timetk::tk_ts(tmp ,
-                           start = start_value,
-                           frequency =   case_when ( freq == "A" ~ 1,
-                                                     freq == "Q" ~ 4,
-                                                     freq == "M" ~ 12),
-                           silent = TRUE)
-
-  approximated <- zoo::na.approx(indy_ts)
-
-  long_form_approx <- as.data.frame (approximated) %>%
-    bind_cols( tmp %>%
-                 select ( all_of("time"))
-               ) %>%
-    pivot_longer( cols = -all_of("time"),
+  long_form_approx <- as.data.frame (approximated) %>% # assinged for easier debugging
+    bind_cols( tmp  %>%
+                 select (
+                   all_of(c("time", "frequency")))
+    )   %>%
+    pivot_longer( cols = -all_of(c("time", "frequency")),
                   names_to = 'geo',
-                  values_to = 'approx') %>%
-    mutate ( method  = "approx") %>%
-    filter ( !is.na("approx"))
+                  values_to = 'value') %>%
+    left_join ( indicator %>%
+                 select (
+                   all_of(c("time", "geo", "estimate", "method", "frequency"))
+                   ),
+                by = c("time", "frequency", "geo")
+               )   %>%
+    mutate ( method  = ifelse(!is.na(value)& method=="missing",
+                              "approx", method))  %>%
+    mutate ( estimate  = ifelse(!is.na(value)& estimate=="missing",
+                              "approx", estimate))
 
+  nrow ( long_form_approx ) == nrow( indicator )
 
+  test_unique_observations(long_form_approx )
 
-  indicator %>%
-    full_join ( anti_join (long_form_approx, indicator )  ) %>%
-    format_return_value( type_approx = "approx") %>%
-    mutate ( method = ifelse ( is.na(.data$value),
-                               "missing", .data$method ))
+  long_form_approx
 
 }
 
 
 #' Last observation carry forward
 #'
-#' @param indicator A tibble created by \code{\link{get_eurostat_indicator}}.
-#' @importFrom timetk tk_ts
-#' @importFrom lubridate year
-#' @importFrom tidyr pivot_wider
-#' @importFrom dplyr select filter left_join mutate case_when
-#' @import zoo
+#' @param indicator A tibble created by \code{\link{get_eurostat_indicator}}. with time, geo,
+#' value, frequency, estimate and method columns.
+#' @importFrom tidyr pivot_wider pivot_longer
+#' @importFrom dplyr select filter left_join mutate bind_cols
+#' @importFrom zoo na.locf
 #' @return A tibble updated with the forward carried values.
 #' @export
 #'
 
 na_locf <- function (indicator) {
 
-
   test_unique_observations(indicator)
 
   freq <- unique(indicator$frequency)
 
   tmp <- indicator %>%
-    select ( all_of(c("time", "geo", "value"))) %>%
+    select ( all_of(c("time", "geo", "value", "frequency" ))) %>%
     pivot_wider( names_from = "geo",
                  values_from = "value")
 
-  start_value <- lubridate::year(min(tmp$time))
+  indicator_ts <- create_time_series(tmp)
+  locf <- zoo::na.locf(indicator_ts)
 
-  indy_ts <- timetk::tk_ts(tmp ,
-                           start = start_value,
-                           frequency =   case_when ( freq == "A" ~ 1,
-                                                     freq == "Q" ~ 4,
-                                                     freq == "M" ~ 12),
-                           silent = TRUE)
-
-  locf <- zoo::na.locf(indy_ts)
-
-  long_form_approx <- as.data.frame (approximated) %>%
-    bind_cols( tmp %>%
-                 select ( all_of("time"))
-    ) %>%
-    pivot_longer( cols = -all_of("time"),
+  long_form_locf <- as.data.frame (locf) %>% # assigned for easier debugging
+    bind_cols( tmp  %>%
+                 select (
+                   all_of(c("time", "frequency")))
+    )   %>%
+    pivot_longer( cols = -all_of(c("time", "frequency")),
                   names_to = 'geo',
-                  values_to = 'approx') %>%
-    mutate ( method  = "locf") %>%
-    filter ( !is.na("approx"))
+                  values_to = 'value') %>%
+    left_join ( indicator %>%
+                  select (
+                    all_of(c("time", "geo", "estimate", "method", "frequency"))
+                  ),
+                by = c("time", "frequency", "geo")
+    )   %>%
+    mutate ( method  = ifelse(!is.na(value)& method=="missing",
+                              "locf", method))  %>%
+    mutate ( estimate  = ifelse(!is.na(value)& estimate=="missing",
+                                "locf", estimate))
 
-  indicator %>%
-    full_join ( anti_join (long_form_approx, indicator )  ) %>%
-    format_return_value( type_approx = "locf") %>%
-    mutate ( method = ifelse ( is.na(.data$value),
-                               "missing", .data$method ))
+  nrow ( long_form_locf ) == nrow( indicator )
+
+  test_unique_observations(long_form_locf )
+
+  long_form_locf
 
 }
 
 #' Next observation carry back
 #'
-#' @param indicator A tibble created by \code{\link{get_eurostat_indicator}}.
-#' @importFrom timetk tk_ts
-#' @importFrom lubridate year
-#' @importFrom tidyr pivot_wider
-#' @importFrom dplyr select filter left_join mutate
+#' @param indicator A tibble created by \code{\link{get_eurostat_indicator}}. with time, geo,
+#' value, frequency, estimate and method columns.
+#' @importFrom tidyr pivot_wider pivot_longer
+#' @importFrom dplyr select filter left_join mutate bind_cols
+#' @importFrom zoo na.locf
 #' @return A tibble updated with the values carried back.
 #' @export
 
@@ -146,36 +166,37 @@ na_nocb <- function (indicator) {
   freq <- unique(indicator$frequency)
 
   tmp <- indicator %>%
-    select ( all_of(c("time", "geo", "value"))) %>%
+    select ( all_of(c("time", "geo", "value", "frequency" ))) %>%
     pivot_wider( names_from = "geo",
                  values_from = "value")
 
-  start_value <- lubridate::year(min(tmp$time))
+  indicator_ts <- create_time_series(tmp)
+  nocb <- zoo::na.locf(indicator_ts, fromLast = TRUE)
 
-  indy_ts <- timetk::tk_ts(tmp ,
-                           start = start_value,
-                           frequency =   case_when ( freq == "A" ~ 1,
-                                                     freq == "Q" ~ 4,
-                                                     freq == "M" ~ 12),
-                           silent = TRUE)
-
-  nocb <- zoo::na.locf(indy_ts, fromLast = TRUE)
-
-  long_form_approx <- as.data.frame (approximated) %>%
-    bind_cols( tmp %>%
-                 select ( all_of("time"))
-    ) %>%
-    pivot_longer( cols = -all_of("time"),
+  long_form_nocb <- as.data.frame (nocb) %>% # assigned for easier debugging
+    bind_cols( tmp  %>%
+                 select (
+                   all_of(c("time", "frequency")))
+    )   %>%
+    pivot_longer( cols = -all_of(c("time", "frequency")),
                   names_to = 'geo',
-                  values_to = 'approx') %>%
-    mutate ( method  = "nocb") %>%
-    filter ( !is.na("approx"))
+                  values_to = 'value') %>%
+    left_join ( indicator %>%
+                  select (
+                    all_of(c("time", "geo", "estimate", "method", "frequency"))
+                  ),
+                by = c("time", "frequency", "geo")
+    )   %>%
+    mutate ( method  = ifelse(!is.na(value)& method=="missing",
+                              "nocb", method))  %>%
+    mutate ( estimate  = ifelse(!is.na(value)& estimate=="missing",
+                                "nocb", estimate))
 
-  indicator %>%
-    dplyr::full_join (dplyr::anti_join (long_form_approx, indicator )  ) %>%
-    format_return_value( type_approx = "nocb") %>%
-    mutate ( method = ifelse ( is.na(.data$value),
-                               "missing", .data$method ))
+  nrow ( long_form_nocb ) == nrow( indicator )
+
+  test_unique_observations(long_form_nocb )
+
+  long_form_nocb
 }
 
 #' Format returned data table
@@ -204,7 +225,11 @@ format_return_value <- function( long_form_approx,
 
 #' Forecast the indicator value
 #'
-#' @param indicator A tibble created by \code{\link{get_eurostat_indicator}}.
+#' @param indicator A tibble created by \code{\link{get_eurostat_indicator}}. with time, geo,
+#' value, frequency, estimate and method columns.
+#' @param forecast_periods The number of expected forecasts.  If set to default \code{NULL},
+#' annual indicators will be forecasted to 3 periods, quarterly to 5 periods,
+#' monthly to 12 periods and daily to 30 periods.
 #' @importFrom timetk tk_ts tk_tbl
 #' @importFrom lubridate year
 #' @importFrom tidyr pivot_wider
@@ -217,45 +242,60 @@ format_return_value <- function( long_form_approx,
 #' @export
 
 
-indicator_forecast <- function (indicator) {
+indicator_forecast <- function (indicator, forecast_periods = NULL) {
 
   test_unique_observations(indicator)
 
   freq <- unique(indicator$frequency)
 
+  message ("this should be rewritten")
+  return(NULL)
+
   tmp <- indicator %>%
-    select ( all_of(c("time", "geo", "value"))) %>%
+    select ( all_of(c("time", "geo", "value", "frequency" ))) %>%
     pivot_wider( names_from = "geo",
                  values_from = "value")
 
-  start_value <- lubridate::year(min(tmp$time))
+  indicator_ts <- create_time_series(tmp)
 
-  indy_ts <- timetk::tk_ts(tmp ,
-                           start = start_value,
-                           frequency =   case_when ( freq == "A" ~ 1,
-                                                     freq == "Q" ~ 4,
-                                                     freq == "M" ~ 12),
-                           silent = TRUE)
+  if (is.null(forecast_periods)) {
+    forecast_periods <- case_when (
+      freq == "A" ~ 3,
+      freq == "M" ~ 12,
+      freq == "Q" ~ 5,
+      freq == "D" ~ 30,
+      TRUE ~ 5)
+  }
 
+  indicator$time
 
-  forecast_periods <- case_when (
-    freq == "A" ~ 3,
-    freq == "M" ~ 12,
-    freq == "Q" ~ 5,
-    freq == "D" ~ 30,
-    TRUE ~ 5)
+  forecasted <- forecast::forecast(indicator_ts, h = forecast_periods )
 
-  forecasted <- forecast::forecast(indy_ts, h = forecast_periods )
-
-  fcast_tbl <- sweep::sw_sweep(forecasted, timetk_idx = TRUE)
+  fcast_tbl <- sweep::sw_sweep(forecasted$forecast, timetk_idx = TRUE)
   methods <- sweep::sw_sweep(forecasted$method, timetk_idx = TRUE)
 
+  forecasted_values <-   matrix (
+    rep(NA_real_, length(fcast_tbl)*forecast_periods),
+    nrow = forecast_periods ) %>%
+    as.data.frame() %>%
+    purrr::set_names ( names(fcast_tbl) )
+
+  sapply ( 1:3, function(x) fcast_tbl[[x]]$x )
+
+  as.zoo(fcast_tbl[[1]]$x)
+
+
   ff <- timetk::tk_tbl(fcast_tbl, silent=TRUE)
+
+  forecasted$forecast
+
+  as.Date(as.integer(ff$Time))
 
   forecast_df <- ff  %>%
     set_names ( snakecase::to_snake_case(names(.))) %>%
     select ( all_of (c("time", "series", "point_forecast"))) %>%
     rename ( geo = .data$series ) %>%
+    mutate ( time = as.Date(time))
     left_join ( data.frame (
       geo = names(methods),
       forecast_method = as.character(methods)
---
+++ tests/testthat/test-approximate.R
@@ -0,0 +1,95 @@
+
+
+
+benelux_linear <- data.frame (
+  time = as.Date (paste0(2010:2020, "-01-01")),
+  NL = seq ( 30, 50, by = 2),
+  BE = seq ( 35, 45, by = 1),
+  LU = seq ( 2, 22, by = 2)
+  )
+
+benelux_linear$NL[c(6, 10:11)] <- NA_real_
+benelux_linear$BE[c(1:3, 10)] <- NA_real_
+benelux_linear$LU[c(4:6)] <- NA_real_
+
+benelux_linear_long <- tidyr::pivot_longer( benelux_linear,
+                                            cols = all_of(c("NL", "BE", "LU")),
+                                            names_to = "geo", values_to = "value") %>%
+  mutate ( estimate = ifelse (is.na(value), "missing", "actual")) %>%
+  mutate ( frequency = "A",
+           method = estimate )
+indicator <- benelux_linear_long
+
+test_that("unique indicator value tests", {
+  expect_true(test_unique_observations (benelux_linear_long))
+  expect_error(test_unique_observations (rbind ( benelux_linear_long, benelux_linear_long )))
+})
+
+approximated_benelux_values <- na_approx (benelux_linear_long)
+
+bl_approximated_values <- approximated_benelux_values  %>%
+  select ( -all_of(c("estimate", "method"))) %>%
+  pivot_wider( names_from = "geo", values_from = "value")
+
+test_that("linear approximation works", {
+  expect_equal(bl_approximated_values %>%
+                 filter ( time == as.Date("2015-01-01") ) %>%
+                 select ( "NL") %>% as.numeric(), 40)
+  expect_equal(bl_approximated_values %>%
+                 filter ( time >=as.Date("2013-01-01"),
+                          time <=as.Date("2015-01-01")) %>%
+                 select ( "LU") %>% unlist() %>% as.numeric(), c(8,10,12))
+  expect_true(is.na(bl_approximated_values$NL[11]))
+})
+
+
+locf_benelux_values <- na_locf(approximated_benelux_values)
+
+bl_locf_values <- locf_benelux_values  %>%
+  select ( -all_of(c("estimate", "method"))) %>%
+  pivot_wider( names_from = "geo", values_from = "value")
+
+locf_labels <- locf_benelux_values %>%
+  filter ( geo  == "BE") %>%
+  select ( all_of(c("time", "value", "method", "estimate")))
+
+
+test_that("locf works", {
+  expect_equal(bl_locf_values %>%
+                 filter ( time >= as.Date("2019-01-01") ) %>%
+                 select ( "NL") %>% unlist() %>% as.numeric(), c(46,46))
+  expect_equal(bl_locf_values %>%
+                 filter ( time < as.Date("2013-01-01")) %>%
+                 select ( "BE") %>% unlist() %>% as.numeric(),rep(NA_real_,3))
+  expect_equal(locf_labels$method, locf_labels$estimate)
+  expect_equal(# Belgian labeling
+               locf_labels$method,
+               c(rep("missing",3), rep("actual", 6), c("approx", "actual"))
+               )
+})
+
+nocb_benelux_values <- na_nocb(locf_benelux_values)
+
+
+bl_nocb_values <- nocb_benelux_values  %>%
+  select ( -all_of(c("estimate", "method"))) %>%
+  pivot_wider( names_from = "geo", values_from = "value")
+
+nocb_labels <- nocb_benelux_values  %>%
+  filter ( geo  == "BE") %>%
+  select ( all_of(c("time", "value", "method", "estimate")))
+
+test_that("nocb works", {
+  expect_equal(bl_nocb_values %>%
+                 filter ( time >= as.Date("2019-01-01") ) %>%
+                 select ( "NL") %>% unlist() %>% as.numeric(), c(46,46))
+  expect_equal(bl_nocb_values %>%
+                 filter ( time < as.Date("2013-01-01")) %>%
+                 select ( "BE") %>% unlist() %>% as.numeric(),
+               rep(38,3))
+  expect_equal(nocb_labels$method, nocb_labels$estimate)
+  expect_equal(# Belgian labeling
+    nocb_labels$method,
+    c(rep("nocb",3), rep("actual", 6), c("approx", "actual"))
+  )
+})
